5f0c5398d82a75d65fdfd858dbdf5baa35cdc34c
mahmut559 <mahmut559@RPC03>
2024-03-19 11:57:14 +0300

    Proje dosyası ekle.

---
+++ uygulama20/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace uygulama20
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBagla = new System.Windows.Forms.Button();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.txtAdsoyad = new System.Windows.Forms.TextBox();
+            this.txtAlan = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBagla
+            // 
+            this.btnBagla.Location = new System.Drawing.Point(229, 74);
+            this.btnBagla.Name = "btnBagla";
+            this.btnBagla.Size = new System.Drawing.Size(75, 23);
+            this.btnBagla.TabIndex = 0;
+            this.btnBagla.Text = "Veri Bağla";
+            this.btnBagla.UseVisualStyleBackColor = true;
+            this.btnBagla.Click += new System.EventHandler(this.btnBagla_Click);
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(83, 28);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(100, 20);
+            this.txtNumara.TabIndex = 1;
+            // 
+            // txtAdsoyad
+            // 
+            this.txtAdsoyad.Location = new System.Drawing.Point(83, 71);
+            this.txtAdsoyad.Name = "txtAdsoyad";
+            this.txtAdsoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdsoyad.TabIndex = 2;
+            // 
+            // txtAlan
+            // 
+            this.txtAlan.Location = new System.Drawing.Point(83, 121);
+            this.txtAlan.Name = "txtAlan";
+            this.txtAlan.Size = new System.Drawing.Size(100, 20);
+            this.txtAlan.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Numara";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AdSoyad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Alan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAlan);
+            this.Controls.Add(this.txtAdsoyad);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.btnBagla);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBagla;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TextBox txtAdsoyad;
+        private System.Windows.Forms.TextBox txtAlan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ uygulama20/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama20
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBagla_Click(object sender, EventArgs e)
+        {
+            Ogrenciler ogrenci = new Ogrenciler();
+            ogrenci.Numara = 571;
+            ogrenci.AdSoyad = "Mahmut Gül";
+            ogrenci.Alan = "Bilişim Teknolojileri";
+            txtNumara.DataBindings.Add("Text",ogrenci,"Numara");
+            txtAdsoyad.DataBindings.Add("Text", ogrenci, "AdSoyad");
+            txtAlan.DataBindings.Add("Text", ogrenci, "Alan");
+        }
+    }
+}
